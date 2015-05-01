f1e5f396b995e88249b1de4ec1bbb76be236ac7a
ctg123 <chaanceg123@gmail.com>
2015-04-30 20:43:50 -0500

    Create Challenge3.py

---
+++ Students/ctg123/Challenge3.py
@@ -0,0 +1,24 @@
+import random
+import numpy as np
+
+# Sum of S = X + Y
+# M = max(X,Y)
+
+NumberTrials = 100000
+sequenceX = []
+sequenceY = []
+sequenceS = []
+sequenceM = []
+
+for TrialIndex in range(0, NumberTrials): 
+	sequneceX.append(random.randint(1,6))
+	sequneceX.append(random.randint(1,6))
+	sequneceX.append(sequenceX[TrialIndex] + sequenceY[TrialIndex])
+	sequneceX.append(max(sequenceX[TrialIndex], sequenceY[TrialIndex]))
+	
+	PMFofSM = np.zeros((13,7))
+	
+	for TrialIndex in range(0,NumberTrials):
+		PMFofSM[sequenceS[TrialIndex], sequenceM[TrialIndex]] += 1
+		
+	PMFofSM /= float(NumberTrials)	
